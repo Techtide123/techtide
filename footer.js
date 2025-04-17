308509c623809f502dad7e94e2072bdf0aff9ed9
Shitansu7205 <shitansukumargochhayat@gmail.com>
2025-04-17 15:19:26 +0530

    Initial commit

---
+++ footer.js
@@ -0,0 +1,94 @@
+document.write(`
+
+
+
+        <!-- Footer Start -->
+        <div class="container-fluid bg-primary text-light footer wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5 px-lg-5">
+                <div class="row g-5">
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Address<span></span></p>
+                        <p><i class="fa fa-map-marker-alt me-3"></i>123, Khandagiri Road
+
+Khandagiri, Bhubaneswar - 751019
+
+Odisha,India</p>
+                        <p><i class="fa fa-phone-alt me-3"> <a href="tel:7205121943" style="color:white;"> +917205121943</a></i></p>
+                        <p><i class="fa fa-envelope me-3"> <a href="mailto:techtide.techtide@gmail.com" style="color:white;">techtide.techtide@gmail.com</a></i></p>
+                        <div class="d-flex pt-2">
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                            <a class="btn btn-outline-light btn-social" href="https://www.instagram.com/techtide_community/"><i class="fab fa-instagram"></i></a>
+                            <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Quick Link<span></span></p>
+                        <a class="btn btn-link" href="index.html">Home </a>
+                        <a class="btn btn-link" href="about.html">About Us</a>
+                        <a class="btn btn-link" href="servive.html">Services</a>
+                        <a class="btn btn-link" href="team.html">Teams</a>
+                        <a class="btn btn-link" href="contact.html">Contact Us</a>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Projects<span></span></p>
+                        <div class="row g-2">
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-11.jpeg" alt="TechTide Image" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-1.jpeg" alt="TechTide Image" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-6.jpeg" alt="TechTide Image" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-2.jpeg" alt="TechTide Image" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-4.jpeg" alt="TechTide Image" style="height:66px;">
+                            </div>
+                            <div class="col-4">
+                                <img class="img-fluid" src="pro-13.png" alt="TechTide Image" style="height:66px;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <p class="section-title text-white h5 mb-4">Tech Tide<span></span></p>
+                        <p>As a team, our mission is not only to create websites but also to foster growth and innovation within our community.</p>
+                        <div class="position-relative w-100 mt-3">
+                            <!-- <form id="contactForm">
+                            <input class="form-control border-0 rounded-pill w-100 ps-4 pe-5" type="email" placeholder="Your Email" style="height: 48px;">
+                            <button type="submit" class="btn shadow-none position-absolute top-0 end-0 mt-1 me-2"><i class="fa fa-paper-plane text-primary fs-4"></i></button>
+                            </form> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container px-lg-5">
+                <div class="copyright">
+                    <div class="row">
+                        <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                            &copy; <a class="border-bottom" href="#">2024</a>, All Right Reserved. 
+							
+							<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "#". Thank you for your support. ***/-->
+							Designed By <a class="border-bottom" href="#">TECH TIDE</a>
+                        </div>
+                        <div class="col-md-6 text-center text-md-end">
+                           <!-- <div class="footer-menu">
+                                <a href="#home">Home</a>
+                                <a href="#about">About Us</a>
+                                <a href="#team">Team</a>
+                                <a href="#services">Services</a> -->
+
+                                <img src="logos.jpeg" alt="tech tide logo" style="width:20%" >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+
+`)
---
+++ header.js
@@ -0,0 +1,33 @@
+document.write(
+    `
+    
+    
+   <!-- Navbar & Hero Start -->
+<div class="container-xxl position-relative p-0">
+    <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+        <a href="/" class="navbar-brand p-0">
+            <img src="logos.jpeg" alt="Tech Tide Logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="fa fa-bars"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav mx-auto py-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="service.html" class="nav-item nav-link">Services</a>
+                <a href="project.html" class="nav-item nav-link">Projects</a>
+                <a href="team.html" class="nav-item nav-link">Our Team</a>
+          
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+            </div>
+            <a href="index.html#contacts" class="btn rounded-pill py-2 px-4 ms-3 d-none d-lg-block">Get Started</a>
+        </div>
+    </nav>
+
+
+</div>
+<!-- Navbar & Hero End -->
+    
+    `
+)
